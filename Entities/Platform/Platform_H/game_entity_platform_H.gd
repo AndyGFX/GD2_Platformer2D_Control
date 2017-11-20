c15274425eef7b15589dfb7591ce957d5db581d6
AndyGFX <o.kollar@gmail.com>
2017-11-20 08:24:24 +0100

    ADD: basic horizontal platform movement

--- Entities/Platform/Platform_H/game_entity_platform_H.gd
+++ Entities/Platform/Platform_H/game_entity_platform_H.gd
@@ -8,12 +8,17 @@ export var item_id = 0
 export var left_end_point = 0
 export var right_end_point = 0
 export var speed = 0
+export var dir = 1
+
+var _pos = null
 
 func _ready():
 	add_to_group("SOLID")
 	platform = get_node(".")
 	connect("area_enter", self, "_on_area_enter")
 	connect("body_enter", self, "_on_body_enter")
+	set_process(true)
+	_pos = get_pos()
 	pass
 
 #------------------------------------------------
@@ -41,3 +46,17 @@ func _on_area_enter(other):
 # ---------------------------------------------------------
 func _on_body_enter(body):
 	pass	
+	
+	
+func _process(delta):
+	
+	
+	translate(Vector2(speed*delta * dir,0))
+	
+	var pos = get_pos()
+	
+	if (pos.x)>=(_pos.x+right_end_point) or (pos.x)<=(_pos.x-left_end_point): 
+		dir=-dir
+	pass
+	
+	
